0bfaf84fd8e0942c1d46782e2c6dce7d3300fbfb
Karim Benkhira <karimbenkhira.contact@gmail.com>
2024-09-11 17:37:00 +0200

    Calcul de la Somme des N Entiers

---
+++ Challenge_10/SommeEntiers.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int sommeNEntiers(int n)
+{
+    int some = 0;
+
+    for(int i = 1;i <= n;i++)
+    {
+        some = some +i;
+    }
+    return some;
+}
+
+
+int main()
+{
+    int n;
+
+    printf("Entrez un nombre entier: \n");
+    scanf("%d",&n);
+
+    if(n <= 0)
+        printf("Veuillez entrer un nombre entier positif.\n");
+    else 
+    {
+        int resultat = sommeNEntiers(n);
+        printf("La somme des %d premiers entiers naturels est: %d\n", n, resultat);
+    }
+}
